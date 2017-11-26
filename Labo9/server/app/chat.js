36148c1143ffdeb7e23b05aaf3325b8213bbd54d
Gregoire GUEMAS <gregoire.guemas@navispeed.eu>
2017-11-26 16:22:53 -0500

    CLean

--- Labo9/server/app/chat.js
+++ Labo9/server/app/chat.js
@@ -1,7 +1,6 @@
 class ChatServer {
     constructor(http) {
         this.io = require('socket.io')(http);
-        this.sockets = [];
         this.io.on('connection', this.onConnection.bind(this));
 
     }
@@ -14,19 +13,13 @@ class ChatServer {
         socket.on('message', this.onMessage.bind(this, socket));
         socket.on('disconnect', this.onDisconnect.bind(this));
         this.io.emit('connection', "A user connected");
-        this.sockets.push(socket);
     }
 
     onMessage(socketEmmiter, message) {
         this.io.emit('message', message)
-        // this.sendMessageToAll('message', message);
     }
 
     onDisconnect(socket) {
-        let s = this.sockets.findIndex((s) => s.id === socket.id);
-        console.log(s, socket.id);
-        // this.sockets.splice(this.sockets.indexOf(socket), 1);
-        console.log(this.sockets.indexOf(s));
         this.io.emit('disconnection', "A user disconnected");
         console.log('user disconnected');
     }
